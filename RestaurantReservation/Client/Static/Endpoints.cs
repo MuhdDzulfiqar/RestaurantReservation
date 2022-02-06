157c6d3a3b0ff8a16b3cd18f23f09f071568ea1f
MUHAMMAD DZULFIQAR BIN MD RASID 2005132B <2005132B@student.tp.edu.sg>
2022-02-06 17:21:23 +0800

    added create and form component into customers

--- RestaurantReservation/Client/Static/Endpoints.cs
+++ RestaurantReservation/Client/Static/Endpoints.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RestaurantReservation.Server.Static
+namespace RestaurantReservation.Client.Static
 {
     public static class Endpoints
     {
